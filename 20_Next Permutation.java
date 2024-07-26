8f736dfd069dcbd3d88ec6edd6a0d8965f22aab9
Marrapu Mani <156051510+Marrapu-Mani@users.noreply.github.com>
2024-07-26 21:28:32 +0530

    Create 20_Next Permutation.java

--- 20_Next Permutation.java
+++ 20_Next Permutation.java
@@ -1,34 +1,37 @@
 class Solution {
+    public void reverse(int[] nums, int l, int r){
+        while(l < r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+
     public void nextPermutation(int[] nums) {
-        int idx = -1;
-        for(int i=nums.length-2; i>=0; i--){
+        // Time Complexity O(n)
+        // Space Complexity O(1)
+        int pivot = -1;
+        for(int i=nums.length-2;  i>=0; i--){
             if(nums[i] < nums[i+1]){
-                idx = i;
+                pivot = i;
                 break;
             }
         }
 
-        if(idx == -1){
-            Arrays.sort(nums);
+        if(pivot == -1){
+            reverse(nums, 0, nums.length-1);
         }else{
-            for(int i=nums.length-1; i>idx; i--){
-                if(nums[i] > nums[idx]){
+            for(int i=nums.length-1; i>pivot; i--){
+                if(nums[i] > nums[pivot]){
                     int temp = nums[i];
-                    nums[i] = nums[idx];
-                    nums[idx] = temp;
+                    nums[i] = nums[pivot];
+                    nums[pivot] = temp;
                     break;
                 }
             }
-
-            int l = idx+1;
-            int r = nums.length-1;
-            while(l < r){
-                int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
-                l++;
-                r--;
-            }
+            reverse(nums, pivot+1, nums.length-1);
         }
     }
 }
