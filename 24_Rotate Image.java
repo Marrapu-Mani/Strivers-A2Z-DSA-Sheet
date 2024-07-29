29156529de79839ec3135d51ade57b90bc487bf0
Marrapu Mani <156051510+Marrapu-Mani@users.noreply.github.com>
2024-07-29 17:14:18 +0530

    Create 24_Rotate Image.java

--- 24_Rotate Image.java
+++ 24_Rotate Image.java
@@ -18,16 +18,34 @@ class Solution {
 
         // Time Complexity O(n^2)
         // Space Complexity O(n^2)  //Beats 68.33%
+        // int n = matrix.length;
+        // int[][] copy = new int[n][n];
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         copy[i][j] = matrix[i][j];
+        //     }
+        // }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         matrix[j][n-1-i] = copy[i][j];
+        //     }
+        // }
+
+        // Time Complexity O(n^2)
+        // Space Complexity O(1)
         int n = matrix.length;
-        int[][] copy = new int[n][n];
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                copy[i][j] = matrix[i][j];
+            for(int j=i; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                matrix[j][n-1-i] = copy[i][j];
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
             }
         }
     }
