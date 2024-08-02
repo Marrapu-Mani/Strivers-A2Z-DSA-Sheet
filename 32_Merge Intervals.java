d876dc8c645c129932018cae12ceecf70c0eec43
Marrapu Mani <156051510+Marrapu-Mani@users.noreply.github.com>
2024-08-02 23:28:09 +0530

    Create 32_Merge Intervals.java

--- 32_Merge Intervals.java
+++ 32_Merge Intervals.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        // Time Complexity O(nlogn)
+        // Space Complexity O(n)
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        List<int[]> merged = new ArrayList<>();
-        int[] prev = intervals[0];
+        List<int[]> merge = new ArrayList<>();
+        int[] prevInterval = intervals[0];
 
-        for (int i = 1; i < intervals.length; i++) {
+        for(int i=1; i<intervals.length; i++){
             int[] interval = intervals[i];
-            if (interval[0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], interval[1]);
-            } else {
-                merged.add(prev);
-                prev = interval;
+            if(interval[0] <= prevInterval[1]){
+                prevInterval[1] = Math.max(prevInterval[1], interval[1]);
+            } else{
+                merge.add(prevInterval);
+                prevInterval = interval;
             }
         }
+        merge.add(prevInterval);
 
-        merged.add(prev);
-
-        return merged.toArray(new int[merged.size()][]);         
+        return merge.toArray(new int[merge.size()][]);         
     }
 }
